2245aad22d973e7c5351cb17223141b05fdfb096
Mikhael as El <scorpiontai22@outlook.com>
2025-01-09 14:04:40 +0700

    Create Oauthnew.vue
    not changes of name file, this is only update

---
+++ Oauthnew.vue
@@ -0,0 +1,41 @@
+
+<script>
+import Cookies from 'js-cookie'
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+
+export default {
+  name: 'oauthGoogle',
+
+  setup() {
+    const route = useRoute();  
+
+    onMounted(() => {
+      const encKey = route.query.encKey;
+      const ivKey = route.query.ivKey;
+      const accessToken = route.query.accessToken;
+      const email = route.query.email
+
+
+
+      if (accessToken) {
+        localStorage.setItem("oauth", accessToken);
+        Cookies.set('tokenUser', email, { expires: 3500, path: '/' });
+      }
+
+      if (encKey && ivKey) {
+    
+    //please store this at indexedDb of encKey and ivKey
+      } else {
+        console.log('encKey or ivKey is missing.');
+      }
+
+      window.location.href = `http://localhost:8080/home`
+    });
+    
+  },
+};
+</script>
+
+<style scoped>
+</style>
